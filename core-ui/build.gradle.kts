ecba742df844d53b4ac624bfd3059392542bdd79
Bhavna Thacker <bt@mega.co.nz>
2023-12-01 01:44:40 +1300

    PASS-149: Move buildscripts out of debug build type fix

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -50,9 +50,9 @@ dependencies {
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.squareup:kotlinpoet:1.14.2")
+    implementation("com.google.code.gson:gson:2.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    debugImplementation("com.squareup:kotlinpoet:1.14.2")
-    debugImplementation("com.google.code.gson:gson:2.9.0")
 }
