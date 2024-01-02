ff9da3b71eb202d32a7b45192a3c85c37710283e
Kevin Gozali <kg@mega.co.nz>
2024-01-02 15:12:50 +0700

    VPN-799 AND - Move dependencies to gradle catalog Shared Core UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,10 +55,11 @@ dependencies {
     implementation(project(":shared:theme"))
     //implementation("mega.android.core:ui:1.0")
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
-    implementation("androidx.compose.material3:material3")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(libs.core.ktx)
+    implementation(libs.lifecycle.runtime.ktx)
+    implementation(libs.activity.compose)
+    implementation(libs.material3)
+    implementation(platform(libs.compose.bom))
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
 }
