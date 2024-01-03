94275f99fdca8eae4bbb2666b80ad429469a60eb
Nikhil Nagori <nna@mega.co.nz>
2024-01-03 18:04:44 +1300

    PASS-234 Add list components for password display screen

--- core-ui/src/main/kotlin/mega/android/core/ui/components/list/List.kt
+++ core-ui/src/main/kotlin/mega/android/core/ui/components/list/List.kt
@@ -1,7 +1,6 @@
 package mega.android.core.ui.components.list
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -35,38 +34,56 @@ import mega.android.core.ui.theme.spacing.LocalSpacing
 import mega.android.core.ui.theme.tokens.TextColor
 
 private val listItemMinHeight = 60.dp
-private val listItemMaxHeight = 88.dp
+private val listItemMaxHeight = 90.dp
 private val headerListItemMinHeight = 36.dp
-private val leadingElementContainerSize = 40.dp
+private val leadingElementContainerSize = 32.dp
 private val vpnCountrySelectedListItemMaxWidth = 382.dp
 private const val TITLE_MAX_LINES = 1
-private const val SUBTITLE_MAX_LINES = 2
 private const val VPN_SUBTITLE_MAX_LINES = 1
+private const val TWO_LINE_LIST_SUBTITLE_MAX_LINES = 1
+private const val MULTI_LINE_LIST_SUBTITLE_MAX_LINES = 2
 
 @Composable
-fun HeaderListItem(
+fun PrimaryHeaderListItem(
     text: String,
     modifier: Modifier = Modifier,
-    onClickListener: () -> Unit = {},
 ) {
-    Box(
+    HeaderListItem(
+        modifier = modifier,
+        text = text,
+        textColor = TextColor.Primary,
+    )
+}
+
+@Composable
+fun SecondaryHeaderListItem(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    HeaderListItem(
+        modifier = modifier,
+        text = text,
+        textColor = TextColor.Secondary,
+    )
+}
+
+@Composable
+private fun HeaderListItem(
+    text: String,
+    textColor: TextColor,
+    modifier: Modifier = Modifier,
+) {
+    MegaText(
         modifier = modifier
             .defaultMinSize(minHeight = headerListItemMinHeight)
             .fillMaxWidth()
-            .background(color = AppTheme.colors.background.surface1)
-            .clickable { onClickListener() }
-            .padding(horizontal = LocalSpacing.current.x16)
-    ) {
-        MegaText(
-            modifier = Modifier
-                .align(Alignment.CenterStart),
-            text = text,
-            textColor = TextColor.Secondary,
-            style = AppTheme.typography.titleSmall,
-            maxLines = TITLE_MAX_LINES,
-            overflow = TextOverflow.Ellipsis
-        )
-    }
+            .padding(horizontal = LocalSpacing.current.x16, vertical = LocalSpacing.current.x8),
+        text = text,
+        textColor = textColor,
+        style = AppTheme.typography.titleSmall,
+        maxLines = TITLE_MAX_LINES,
+        overflow = TextOverflow.Ellipsis
+    )
 }
 
 @Composable
@@ -84,6 +101,24 @@ fun OneLineListItem(
     leadingElement = leadingElement,
     trailingElement = trailingElement,
     onClickListener = onClickListener
+)
+
+@Composable
+fun TwoLineListItem(
+    title: String,
+    subtitle: String,
+    modifier: Modifier = Modifier,
+    leadingElement: (@Composable (BoxScope.() -> Unit))? = null,
+    trailingElement: (@Composable (() -> Unit))? = null,
+    onClickListener: () -> Unit = {},
+) = ListItem(
+    modifier = modifier.heightIn(min = listItemMinHeight, max = listItemMaxHeight),
+    title = title,
+    subtitle = subtitle,
+    leadingElement = leadingElement,
+    trailingElement = trailingElement,
+    onClickListener = onClickListener,
+    subtitleMaxLines = TWO_LINE_LIST_SUBTITLE_MAX_LINES,
 )
 
 @Composable
@@ -118,7 +153,7 @@ fun VpnSelectedCountryListItem(
     title = title,
     subtitle = subtitle,
     subtitleMaxLines = VPN_SUBTITLE_MAX_LINES,
-            leadingElement = {
+    leadingElement = {
         Icon(
             painter = painterResource(id = countryFlagRes),
             contentDescription = null,
@@ -146,7 +181,7 @@ private fun ListItem(
     leadingElement: (@Composable (BoxScope.() -> Unit))? = null,
     trailingElement: (@Composable (() -> Unit))? = null,
     onClickListener: () -> Unit = {},
-    subtitleMaxLines: Int = SUBTITLE_MAX_LINES,
+    subtitleMaxLines: Int = MULTI_LINE_LIST_SUBTITLE_MAX_LINES,
 ) {
     Row(
         modifier = modifier
@@ -218,10 +253,10 @@ private fun OneLineListItemPreview() {
 @CombinedThemePreviews
 private fun TwoLineListItemPreview() {
     AndroidThemeForPreviews {
-        MultiLineListItem(
-            modifier = Modifier,
+        TwoLineListItem(
             title = "List item",
-            subtitle = "Supporting line text lorem ipsum lorem ipsum"
+            subtitle = "Supporting line text lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+            modifier = Modifier
         )
     }
 }
@@ -330,7 +365,15 @@ private fun MultiLineListItemPreviewWithElements() {
 @CombinedThemePreviews
 fun HeaderListItemPreview() {
     AndroidThemeForPreviews {
-        HeaderListItem(text = "Header text")
+        SecondaryHeaderListItem(text = "Header text")
+    }
+}
+
+@Composable
+@CombinedThemePreviews
+fun PrimaryHeaderListItemPreview() {
+    AndroidThemeForPreviews {
+        PrimaryHeaderListItem(text = "Header text")
     }
 }
 
