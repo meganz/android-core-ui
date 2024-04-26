478c017cec7221a453a4fc3119bc3690f61836b1
Ming Fan <mfn@mega.co.nz>
2024-04-26 16:45:19 +1200

    VPN-1344 Migrate snackbar to core ui module

--- core-ui/src/main/kotlin/mega/android/core/ui/components/MegaSnackbar.kt
+++ core-ui/src/main/kotlin/mega/android/core/ui/components/MegaSnackbar.kt
@@ -1,34 +1,49 @@
 package mega.android.core.ui.components
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Snackbar
-import androidx.compose.material3.SnackbarData
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import mega.android.core.ui.theme.AppTheme
 import mega.android.core.ui.theme.spacing.LocalSpacing
+import mega.android.core.ui.model.MegaSnackBarVisuals
+import mega.android.core.ui.model.calculatePadding
 
 private val snackBarWidthTablet = 344.dp
 
 @Composable
 fun MegaSnackbar(
-    snackbarData: SnackbarData,
+    snackBarHostState: SnackbarHostState,
     isTablet: Boolean = false
 ) {
-    val spacing = LocalSpacing.current
-    val modifier = if (isTablet) Modifier
-        .width(snackBarWidthTablet)
-        .padding(bottom = spacing.x12) else Modifier.padding(
-        bottom = spacing.x12,
-        start = spacing.x8,
-        end = spacing.x8
-    )
-    Snackbar(
-        modifier = modifier,
-        snackbarData = snackbarData,
-        containerColor = AppTheme.colors.components.toastBackground,
-        contentColor = AppTheme.colors.background.pageBackground
-    )
+    SnackbarHost(snackBarHostState) { data ->
+        val customVisuals = data.visuals as? MegaSnackBarVisuals
+        val snackBarAttributes = customVisuals?.attributes
+        val horizontalSpace = if (isTablet) 0.dp else LocalSpacing.current.x8
+        val snackBarPadding = snackBarAttributes?.calculatePadding(isTablet)
+            ?: PaddingValues(
+                bottom = LocalSpacing.current.x12,
+                start = horizontalSpace,
+                end = horizontalSpace
+            )
+
+        Snackbar(
+            modifier = if (isTablet) {
+                Modifier
+                    .width(snackBarWidthTablet)
+                    .padding(snackBarPadding)
+            } else {
+                Modifier.padding(snackBarPadding)
+            },
+            snackbarData = data,
+            containerColor = AppTheme.colors.components.toastBackground,
+            contentColor = AppTheme.colors.background.pageBackground,
+            actionColor = AppTheme.colors.link.primary
+        )
+    }
 }
---
+++ core-ui/src/main/kotlin/mega/android/core/ui/model/MegaSnackBarVisuals.kt
@@ -0,0 +1,12 @@
+package mega.android.core.ui.model
+
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarVisuals
+
+class MegaSnackBarVisuals(
+    override val actionLabel: String?,
+    override val duration: SnackbarDuration,
+    override val message: String,
+    override val withDismissAction: Boolean,
+    val attributes: SnackBarAttributes
+) : SnackbarVisuals
