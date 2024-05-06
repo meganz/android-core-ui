f43898d555c8227259d4bbeed53f3c59469ece73
Robin Shi <rsh@mega.co.nz>
2024-05-07 10:16:48 +1200

    AND-18718 add UTC timestamp to version of core-ui

--- buildSrc/src/main/kotlin/mega/android/core/ui/gradle/Util.kt
+++ buildSrc/src/main/kotlin/mega/android/core/ui/gradle/Util.kt
@@ -2,10 +2,19 @@ package mega.android.core.ui.gradle
 
 import org.gradle.api.Project
 import java.io.File
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 const val VERSION_FILE = "version.txt"
 
-fun readLibVersion(project: Project): String =
+/**
+ * Read version from version.txt file
+ *
+ * @param project
+ * @return
+ */
+private fun getVersionFromFile(project: Project): String =
     try {
         File("${project.rootDir.absolutePath}/$VERSION_FILE").readText()
     } catch (e: Exception) {
@@ -23,3 +32,30 @@ fun writeLibVersion(project: Project, version: String) {
     }
 }
 
+/**
+ * get Patch of the version
+ *
+ * @return
+ */
+private fun getPatch(): String =
+    System.getenv(PATCH_VERSION) ?: OffsetDateTime.now(ZoneOffset.UTC)
+        .format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))
+
+/**
+ * get the full version string
+ *
+ * @param project
+ * @return
+ */
+fun getLibVersion(project: Project): String {
+    val version = getVersionFromFile(project)
+    val patch = getPatch()
+    val snapshot = System.getenv(SNAPSHOT).orEmpty()
+    return "${version}.${patch}$snapshot"
+}
+
+// environment variable keys
+const val SNAPSHOT = "SNAPSHOT"
+const val PATCH_VERSION = "PATCH_VERSION"  // patch in the version string
+const val LIB_COMMIT = "LIB_COMMIT"  // commit of current lib
+const val GITLAB_USER_NAME = "gitlabUserName" // user who triggered the build
