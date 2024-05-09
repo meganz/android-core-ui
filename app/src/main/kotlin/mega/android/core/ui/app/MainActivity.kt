c7176dc7a1dab34050deff515eaa8bd74c2cce02
Bhavna Thacker <bt@mega.co.nz>
2024-05-09 23:48:38 +1200

    AND-18752: Test auto upgrade of version and published version on MR merge

--- app/src/main/kotlin/mega/android/core/ui/app/MainActivity.kt
+++ app/src/main/kotlin/mega/android/core/ui/app/MainActivity.kt
@@ -45,7 +45,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // A surface container using the 'background' color from the Core UI theme
+            // A surface container using the 'background' color from the theme
             CoreUITheme(isDark = isSystemInDarkTheme()) {
                 ThemedSurface {
                     MainComposeView()
