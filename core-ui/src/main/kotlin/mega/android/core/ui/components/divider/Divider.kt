78933e29f8da053dcf3e0ecb8ff0f7c7ecfef72a
Nikhil Nagori <nna@mega.co.nz>
2024-07-30 18:31:43 +1200

    PASS-807 Add vertical divider

--- core-ui/src/main/kotlin/mega/android/core/ui/components/divider/Divider.kt
+++ core-ui/src/main/kotlin/mega/android/core/ui/components/divider/Divider.kt
@@ -1,11 +1,15 @@
 package mega.android.core.ui.components.divider
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import mega.android.core.ui.preview.CombinedThemePreviews
+import mega.android.core.ui.theme.AndroidThemeForPreviews
 import mega.android.core.ui.theme.AppTheme
 
 @Composable
@@ -13,6 +17,15 @@ fun SubtleDivider(modifier: Modifier = Modifier) {
     HorizontalDivider(
         modifier = modifier,
         thickness = 1.dp,
+        color = AppTheme.colors.border.subtle
+    )
+}
+
+@Composable
+fun SubtleVerticalDivider(thickness: Dp, modifier: Modifier = Modifier) {
+    VerticalDivider(
+        modifier = modifier,
+        thickness = thickness,
         color = AppTheme.colors.border.subtle
     )
 }
@@ -27,13 +40,42 @@ fun StrongDivider(modifier: Modifier = Modifier) {
 }
 
 @Composable
+fun StrongVerticalDivider(thickness: Dp, modifier: Modifier = Modifier) {
+    VerticalDivider(
+        modifier = modifier,
+        thickness = thickness,
+        color = AppTheme.colors.border.strong
+    )
+}
+
+@Composable
 @CombinedThemePreviews
 private fun SubtleDividerPreview() {
-    SubtleDivider(modifier = Modifier.fillMaxWidth())
+    AndroidThemeForPreviews {
+        SubtleDivider(modifier = Modifier.fillMaxWidth())
+    }
 }
 
 @Composable
 @CombinedThemePreviews
 private fun StrongDividerPreview() {
-    StrongDivider(modifier = Modifier.fillMaxWidth())
+    AndroidThemeForPreviews {
+        StrongDivider(modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Composable
+@CombinedThemePreviews
+private fun SubtleVerticalDividerPreview() {
+    AndroidThemeForPreviews {
+        SubtleVerticalDivider(thickness = 2.dp, modifier = Modifier.height(30.dp))
+    }
+}
+
+@Composable
+@CombinedThemePreviews
+private fun StrongVerticalDividerPreview() {
+    AndroidThemeForPreviews {
+        StrongVerticalDivider(thickness = 2.dp, modifier = Modifier.height(30.dp))
+    }
 }
