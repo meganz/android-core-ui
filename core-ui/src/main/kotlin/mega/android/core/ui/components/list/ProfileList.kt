4b403698967549dabe13ccf62f288dd3989e6572
mega-vap <vap@mega.co.nz>
2024-12-13 16:10:15 +0700

    T20811900/PASS-1491: Add line style for profile list item

--- core-ui/src/main/kotlin/mega/android/core/ui/components/list/ProfileList.kt
+++ core-ui/src/main/kotlin/mega/android/core/ui/components/list/ProfileList.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import mega.android.core.ui.components.MegaText
 import mega.android.core.ui.components.profile.LargeProfilePicture
 import mega.android.core.ui.preview.CombinedThemePreviews
@@ -18,12 +19,33 @@ import mega.android.core.ui.theme.spacing.LocalSpacing
 import mega.android.core.ui.theme.values.TextColor
 import java.io.File
 
+class ProfileListTextLineStyle(
+    val overflow: TextOverflow,
+    val maxLines: Int,
+    val minLines: Int
+)
+
+object ProfileListTextLineStyleDefaults {
+
+    fun lineStyle(
+        overflow: TextOverflow = TextOverflow.Clip,
+        maxLines: Int = Int.MAX_VALUE,
+        minLines: Int = 1,
+    ): ProfileListTextLineStyle = ProfileListTextLineStyle(
+        overflow = overflow,
+        maxLines = maxLines,
+        minLines = minLines
+    )
+}
+
 @Composable
 fun ProfileListItem(
     fullName: String,
     email: String,
     avatarFile: File?,
     modifier: Modifier = Modifier,
+    fullNameLineStyle: ProfileListTextLineStyle = ProfileListTextLineStyleDefaults.lineStyle(),
+    emailLineStyle: ProfileListTextLineStyle = ProfileListTextLineStyleDefaults.lineStyle(),
     avatarColor: Color = AppTheme.colors.background.surface3,
 ) {
     Row(
@@ -45,11 +67,17 @@ fun ProfileListItem(
             MegaText(
                 text = fullName,
                 textColor = TextColor.Primary,
+                overflow = fullNameLineStyle.overflow,
+                maxLines = fullNameLineStyle.maxLines,
+                minLines = fullNameLineStyle.minLines,
                 style = AppTheme.typography.titleMedium,
             )
             MegaText(
                 text = email,
                 textColor = TextColor.Secondary,
+                overflow = emailLineStyle.overflow,
+                maxLines = emailLineStyle.maxLines,
+                minLines = emailLineStyle.minLines,
                 style = AppTheme.typography.bodyMedium,
             )
         }
@@ -75,6 +103,26 @@ fun ProfileListItemLongTextPreview() {
         ProfileListItem(
             fullName = "Very very very long FirstName Very very very long LastName",
             email = "Very very very long Very very very long Very very very long example@example.com",
+            avatarFile = null
+        )
+    }
+}
+
+@CombinedThemePreviews
+@Composable
+fun ProfileListItemLongTextWithLineStylePreview() {
+    AndroidThemeForPreviews {
+        ProfileListItem(
+            fullName = "Very very very long FirstName Very very very long LastName",
+            fullNameLineStyle = ProfileListTextLineStyleDefaults.lineStyle(
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1
+            ),
+            email = "Very very very long Very very very long Very very very long example@example.com",
+            emailLineStyle = ProfileListTextLineStyleDefaults.lineStyle(
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1
+            ),
             avatarFile = null
         )
     }
