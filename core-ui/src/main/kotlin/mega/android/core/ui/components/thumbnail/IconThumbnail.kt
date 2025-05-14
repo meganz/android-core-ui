e0a22d021dae7aed1965e11b28ac9b5a80f34508
mega-vap <vap@mega.co.nz>
2025-05-14 13:50:32 +0700

    PASS-2291: Add radius to icon thumbnail

--- core-ui/src/main/kotlin/mega/android/core/ui/components/thumbnail/IconThumbnail.kt
+++ core-ui/src/main/kotlin/mega/android/core/ui/components/thumbnail/IconThumbnail.kt
@@ -3,23 +3,30 @@ package mega.android.core.ui.components.thumbnail
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import mega.android.core.ui.R
 import mega.android.core.ui.components.image.MegaIcon
 import mega.android.core.ui.preview.CombinedThemePreviews
 import mega.android.core.ui.theme.AndroidThemeForPreviews
+import mega.android.core.ui.theme.spacing.LocalSpacing
 import mega.android.core.ui.theme.values.IconColor
 import mega.android.core.ui.tokens.theme.DSTokens
 
 @Composable
 fun IconThumbnail(
     modifier: Modifier = Modifier,
+    radius: Dp = LocalSpacing.current.x4,
     content: @Composable BoxScope.() -> Unit
 ) {
     Box(
-        modifier = modifier.background(color = DSTokens.colors.background.surface3),
+        modifier = modifier.background(
+            color = DSTokens.colors.background.surface3,
+            shape = RoundedCornerShape(size = radius)
+        ),
         content = content
     )
 }
