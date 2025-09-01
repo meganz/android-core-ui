d29424ea9a75b4b05ad86f361ac1632a155e9dfc
Pau Dominkovics <pau@mega.co.nz>
2025-09-01 14:36:24 +0200

    AND-21190: Bound ripple to dialog button bounds

--- core-ui/src/main/kotlin/mega/android/core/ui/components/dialogs/DialogButtons.kt
+++ core-ui/src/main/kotlin/mega/android/core/ui/components/dialogs/DialogButtons.kt
@@ -7,26 +7,24 @@ import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import mega.android.core.ui.preview.CombinedThemePreviews
 import mega.android.core.ui.theme.AndroidThemeForPreviews
 import mega.android.core.ui.theme.AppTheme
-import mega.android.core.ui.tokens.theme.DSTokens
 import mega.android.core.ui.theme.spacing.LocalSpacing
+import mega.android.core.ui.tokens.theme.DSTokens
 
 @Composable
 fun DialogButton(
@@ -39,13 +37,12 @@ fun DialogButton(
     val isFocused by mutableInteractionSource.collectIsFocusedAsState()
     val borderColor = if (isFocused) DSTokens.colors.focus.colorFocus else Color.Unspecified
     val pressedBackgroundColor = DSTokens.colors.button.primaryPressed
-    var componentWidth by remember { mutableStateOf(0.dp) }
-    val density = LocalDensity.current
 
     Box(
         modifier = Modifier
             .wrapContentSize()
             .focusRequester(focusRequester)
+            .clip(RoundedCornerShape(8.dp))
             .then(
                 if (enabled) {
                     Modifier.clickable(
@@ -72,11 +69,6 @@ fun DialogButton(
                 horizontal = LocalSpacing.current.x16,
                 vertical = LocalSpacing.current.x12
             )
-            .onGloballyPositioned {
-                componentWidth = with(density) {
-                    it.size.width.toDp()
-                }
-            }
     ) {
         Text(
             text = buttonText,
