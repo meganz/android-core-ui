c771a6f9489d36dc6a2c15f2751de2b5f7cb003d
Kevin Gozali <kg@mega.co.nz>
2025-09-23 09:34:29 +0700

    SAO-3097 Adjust cursor position for TextInputField

--- core-ui/src/main/kotlin/mega/android/core/ui/components/inputfields/LabelledTextInputWithAction.kt
+++ core-ui/src/main/kotlin/mega/android/core/ui/components/inputfields/LabelledTextInputWithAction.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.autofill.ContentType
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.ImeOptions
 import androidx.compose.ui.text.input.KeyboardCapitalization
@@ -104,7 +105,7 @@ fun LabelledTextInputWithAction(
         keyboardType = keyboardType,
         imeAction = imeAction,
         capitalization = capitalization,
-        text = TextFieldValue(text),
+        text = TextFieldValue(text, TextRange(text.length)),
         successText = successText,
         errorText = errorText,
         warningText = warningText,
@@ -278,7 +279,7 @@ private fun LabelledTextInputWithActionPreview() {
                 onFocusChanged = {}
             )
             LabelledTextInputWithAction(
-                textValue = TextFieldValue("Text"),
+                textValue = TextFieldValue("Text", TextRange(4)),
                 keyboardType = KeyboardType.Text,
                 modifier = Modifier.padding(LocalSpacing.current.x16),
                 imeAction = ImeAction.Done,
@@ -306,7 +307,7 @@ private fun LabelledTextInputWithActionPreview() {
                 }
             )
             LabelledTextInputWithAction(
-                textValue = TextFieldValue("Text"),
+                textValue = TextFieldValue("Text", TextRange(4)),
                 keyboardType = KeyboardType.Text,
                 modifier = Modifier.padding(LocalSpacing.current.x16),
                 imeAction = ImeAction.Done,
